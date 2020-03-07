a4b085170a5785a4763809c25114dc9805df1062
Felix Hauer <hauerf98@gmail.com>
2020-03-07 10:33:29 +0100

    fixed imputing of categorial data

--- udacity/pandas_templates.py
+++ udacity/pandas_templates.py
@@ -41,6 +41,7 @@ def impute_knn():
     print(imp.transform(X_test))
 
 
+# WARNING: biased imputing!!!!! dot not use
 def impute_integers_categories():
     data = {'Attribute': [1,1,1,2,2, np.nan],
             'Missing': [0.56, 0.45, 0.46, np.nan, 0.35, 0.48]}
@@ -57,4 +58,46 @@ def impute_integers_categories():
 
 
     print(df)
+
+
+# imputing categorical and numeric data separate, merging them back together and create dummies
+def impute_by_dtype():
+    data = {'Attribute': [1,1,1,2,2, np.nan],
+            'Missing': [0.56, 0.45, 0.46, np.nan, 0.35, 0.48]}
+    df = pd.DataFrame(data)
+    df['Attribute'] = df['Attribute'].astype('category')
+
+
+    print(df)
+    print(df.dtypes)
+    print("")
+
+    # selecting only category columns and apply SimpleImputer for categories
+    df_category = df.select_dtypes(include='category')
+    imp = SimpleImputer(strategy="most_frequent")
+    imp.fit(df_category)
+    df_category = pd.DataFrame(imp.transform(df_category), columns=df_category.columns)
+    df_category = df_category.astype('int')
+    df_category = df_category.astype('category')
+
+
+    # selecting only numeric columns and apply SimpleImputer for categories
+    df_float = df.select_dtypes(include='float64')
+    imp = KNNImputer(n_neighbors=2, weights="uniform")
+    imp.fit(df_float)
+    df_float = pd.DataFrame(imp.transform(df_float), columns=df_float.columns)
+
+
+
+    #mergin whole df back together
+    df_imputed = pd.concat([df_category, df_float], axis=1)
+
+    #creating dummies
+    df_final = pd.get_dummies(df_imputed)
+
+    print(df_final)
+
+
+
+
 
